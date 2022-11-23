3f95ec03d68458c903fe3454ebfd0ad5decb1b5e
cuzamota <kimjs3061@naver.com>
2022-11-23 22:01:23 +0900

    20221123 1:48:52 backend

---
+++ backend/src/main/java/org/africalib/gallery/backend/entity/Item.java
@@ -0,0 +1,27 @@
+package org.africalib.gallery.backend.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동증가
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 100)
+    private String imgPath;
+
+    @Column
+    private String price;
+
+    @Column
+    private int discountPer;
+}
